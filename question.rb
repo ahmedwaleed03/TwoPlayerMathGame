9d527c23bbdacabc950974f8fa2f1cd69b39ca06
Waleed Ahmed <baloch.waleedahmed@gmail.com>
2023-10-16 15:16:49 -0400

    Created the question class

---
+++ question.rb
@@ -0,0 +1,10 @@
+class Question
+  attr_reader :text, :answer
+
+  def initialize
+    num1 = rand(1..20)
+    num2 = rand(1..20)
+    @text = "What does #{num1} plus #{num2} equal?"
+    @answer = num1 + num2
+  end
+end
